7d192f8260fba572e2d8760fa282a0fef80ab9f2
Keitaro Koga <pokutin21@gmail.com>
2021-04-05 10:21:44 +0900

    make fn run

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use clap::Clap;
+use std::fs::File;
+use std::io::{stdin, BufRead, BufReader};
+
+#[derive(Clap, Debug)]
+#[clap(
+  name = "My RPN program",
+  version = "1.0.0",
+  author = "Keitaro Koga",
+  about = "Super awsome sample RPN calculator"
+)]
+
+struct Opts {
+  // Sets the level of verbosity
+  #[clap(short, long)]
+  verbose: bool,
+
+  // Formulas written in RPN
+  #[clap(name = "FILE")]
+  formula_file: Option<String>,
+}
+
+fn main() {
+
+  let opts = Opts::parse();
+
+  if let Some(path) = opts.formula_file {
+    let f = File::open(path).unwrap();
+    let reader = BufReader::new(f);
+
+    run(reader, opts.verbose);
+  } else {
+    let stdin = stdin();
+    let reader = stdin.lock();
+    run(reader, opts.verbose);
+  }
+}
+
+fn run<R: BufRead>(reader: R, verbose: bool) {
+  for line in reader.lines() {
+    let line = line.unwrap();
+    println!("{}", line);
+  }
+}
